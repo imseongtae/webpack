24b13055a858b328e7af30395e0c484de96b6853
imseongtae <dev.haemil@gmail.com>
2021-07-06 12:00:33 +0900

    feat: Add modules

---
+++ basic-understanding-of-webpack/src/app.js
@@ -0,0 +1,13 @@
+import './styles/style.css';
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(1, 2));
+
+
+console.log(VERSION) // 'v.1.2.3'
+console.log(PRODUCTION) // true
+console.log(MAX_COUNT) // 999
+console.log(api.domain) // 'http://dev.api.domain.com'
---
+++ basic-understanding-of-webpack/src/math.js
@@ -0,0 +1,12 @@
+var math = math || {} // math 네임스페이스
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+(function name() {
+  function sum(a, b) {
+    return a + b;
+  }
+  math.sum = sum;
+})();
